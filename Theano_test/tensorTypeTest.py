b1cc714808f977298e971e2842f473fcd87f4400
daluzi <1210415250@qq.com>
2019-10-28 15:54:31 +0800

    tensor type

---
+++ Theano_test/tensorTypeTest.py
@@ -0,0 +1,10 @@
+# _*_ coding: utf-8 _*_
+# @Author   : daluzi
+# @time     : 2019/10/28 15:45
+# @File     : tensorTypeTest.py
+# @Software : PyCharm
+
+import theano
+import numpy as np
+import theano.tensor as T
+
